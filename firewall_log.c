ac06c5d6309c58ae6e0a954e7f9be48e96f5e4aa
Raluca Stanciu <ralucstanciu@gmail.com>
2025-11-04 19:47:49 -0500

    add: more details to the log

--- firewall_log.c
+++ firewall_log.c
@@ -4,7 +4,7 @@
 #include <linux/netfilter_ipv4.h>
 #include <linux/ip.h>
 
-static struct nf_hook_ops nfho;
+static struct nf_hook_ops nfho[5];
 
 const char *hook_name(unsigned int hooknum) {
     switch (hooknum) {
@@ -30,33 +30,49 @@ const char *proto_name(unsigned int proto) {
 
 unsigned int hook_func(void *priv,
 		struct sk_buff *skb,
-		const struct nf_hook_state *state		
-		){
+		const struct nf_hook_state *state){
 
 	struct iphdr *iph = ip_hdr(skb);
 
 	if(!iph) return NF_ACCEPT;
 
-	printk(KERN_INFO "[fwlog] Hook: %s, Protocol: %s, Src: %pI4, Dst: %pI4\n", hook_name(state->hook), proto_name(iph->protocol), &iph->saddr, &iph->daddr);
+	printk(KERN_INFO "[fwlog] Hook: %s, Protocol: %s, Src: %pI4, Dst: %pI4\n",
+		hook_name(state->hook), proto_name(iph->protocol), &iph->saddr, &iph->daddr);
 	
 	return NF_ACCEPT;
 }
-		
+
+
 static int __init logger_init(void){
 
-	nfho.hook = hook_func;
-	nfho.pf = PF_INET;
-	nfho.hooknum = NF_INET_PRE_ROUTING;
-	nfho.priority = NF_IP_PRI_FIRST;
+    int i;
+    unsigned int hooks[] = {
+        NF_INET_PRE_ROUTING,
+        NF_INET_LOCAL_IN,
+        NF_INET_FORWARD,
+        NF_INET_LOCAL_OUT,
+        NF_INET_POST_ROUTING
+    };
 
-	nf_register_net_hook(&init_net, &nfho);
-	printk(KERN_INFO "Firewall logger loaded.\n");
-	return 0;
+    for (i = 0; i < 5; i++) {
+        nfho[i].hook = hook_func;
+        nfho[i].pf = PF_INET;
+        nfho[i].hooknum = hooks[i];
+        nfho[i].priority = NF_IP_PRI_FIRST;
+        nf_register_net_hook(&init_net, &nfho[i]);
+    }
+
+    printk(KERN_INFO "Firewall logger loaded. Hooks registered for all IPv4 stages.\n");
+    return 0;
 }
 
-static void __exit logger_exit(void){
-	nf_unregister_net_hook(&init_net, &nfho);
-	printk(KERN_INFO "Firewall logger unloaded.\n");
+static void __exit logger_exit(void)
+{
+    int i;
+    for (i = 0; i < 5; i++)
+        nf_unregister_net_hook(&init_net, &nfho[i]);
+
+    printk(KERN_INFO "Firewall logger unloaded.\n");
 }
 
 module_init(logger_init);
